9f91bb95ccbf9d335a5e33c912bc9e0d5e5c1a0c
Saif Alhaider <saifharith90@gmail.com>
2022-10-25 15:36:24 +0300

    added simple drawer

--- lib/view/Home/home_main.dart
+++ lib/view/Home/home_main.dart
@@ -3,37 +3,54 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:taxi_baghdad/view/Home/home_dashboard/dashboard_main.dart';
+import 'package:taxi_baghdad/view/widgets/custom_text.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
   static GoogleMapController? mapController;
   static final Completer<GoogleMapController> _controller = Completer();
   static CameraPosition kGooglePlex = const CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+    target: LatLng(33.234269, 44.334756),
     zoom: 14.4746,
   );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("main screen"),
-      // ),
+      appBar: AppBar(
+        title: Text("main screen"),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding:  EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+            child: Column(
+              children: [
+                ListTile(
+                  onTap: (){},
+                  title: CustomText(text: "Profile",size: 20,),
+                ),
+                ListTile(
+                  onTap: (){},
+                  title: CustomText(text: "Home",size: 20,),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
       body: Stack(children: [
         GoogleMap(
-              mapType: MapType.normal,
-              myLocationButtonEnabled: true,
-              initialCameraPosition: kGooglePlex,
-              onMapCreated: (GoogleMapController controller) {
-                _controller.complete(controller);
-                mapController = controller;
-              },
-            ),
-            const Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-                child: Dashboard()),
+          mapType: MapType.normal,
+          myLocationButtonEnabled: true,
+          myLocationEnabled: true,
+          initialCameraPosition: kGooglePlex,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+            mapController = controller;
+          },
+        ),
+        const Positioned(left: 0, right: 0, bottom: 0, child: Dashboard()),
       ]),
     );
   }
